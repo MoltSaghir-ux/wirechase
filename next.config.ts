5df612fdfe57c9e4913e66380662da209257bae0
MoltSaghir <moltsaghir@MoltSaghirs-MacBook-Pro.local>
2026-03-05 15:51:25 -0500

    fix(sentry): remove withSentryConfig wrapper causing CSRF errors, guard init on DSN presence

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from 'next'
-import { withSentryConfig } from '@sentry/nextjs'
 
 const nextConfig: NextConfig = {
   /* config options here */
 }
 
-export default withSentryConfig(nextConfig, {
-  // Sentry webpack plugin options
-  org: process.env.SENTRY_ORG,
-  project: process.env.SENTRY_PROJECT,
-  silent: true, // suppress output in CI
-  widenClientFileUpload: true,
-  sourcemaps: { disable: false },
-  disableLogger: true,
-  automaticVercelMonitors: false,
-})
+export default nextConfig
